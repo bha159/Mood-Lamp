8ca8f98c7a1b123a62657dcda9567f24b697c957
bha159 <bharat.159753@gmail.com>
2017-10-04 03:38:12 +0630

    Minor mistakes rectified

--- arduin.m
+++ arduin.m
@@ -1,21 +1,25 @@
-function [] = arduino(freq)
-clearvars a;
-a = arduino('com4');
+function [] = arduin(a,freq)
+%This is self made function.
+%This function is made for using arduino.
+%This function lits up the LEDs according to frequencies it get.
+
+fr = 2;
+fb = 3;
 configurePin(a,'D8');
 configurePin(a,'D9');
-configurePin(a,'D10');
+configurePin(a,'D13');
 
-for i=:len(freq)
+for i=1:length(freq)
     f = freq(i);
-    if freq <= fr
+    if (f <= fr)
         disp('RED');
         writeDigitalPin(a, 'D8', 1);
     else
-        if freq<=fb && freq>=fr
+        if (f<=fb) && (f>=fr)
             disp('GREEN');
-            writeDigitalPin(a, 'D9', 1);
+            writeDigitalPin(a, 'D13', 1);
         else 
-            if freq>=fb
+            if (f>=fb)
                 disp('Blue');
                 writeDigitalPin(a, 'D10', 1);
             else
--- test.m
+++ test.m
@@ -1,9 +1,10 @@
-a = input('Press 1 to start recording\n');
 rec=audiorecorder(8000,8,1);
+ard = arduino('com3');
+a = input('Press 1 to start recording\n');
 while(1)
     recordblocking(rec,0.1);
     a=getaudiodata(rec);
     b = fft(transpose(a+1));
     [~,freq] = pwelch(b);
-    arduino(freq);
+    arduin(ard,freq);
 end
